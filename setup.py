0aaead6728cc5380832ebe353ffcba31cb7be71d
Mike Smith <michaesm@marine.rutgers.edu>
2016-11-08 11:04:11 -0500

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='plot-nc-ooi',
+    version='1',
+    packages=['functions'],
+    url='https://github.com/ooi-data-review/plot-nc-ooi',
+    license='',
+    author='Mike Smith',
+    author_email='michaesm@marine.rutgers.edu',
+    description='This is a python script utilizing python to plot Ocean Observatories Initiative (OOI) netCDF datasets.'
+)
