7c8cf2e009b700cf386ce2760689b343f7cf774f
Mike Smith <michaesm@marine.rutgers.edu>
2017-04-05 12:51:59 -0400

    Script updates

--- scripts/plot_interactive_ts.py
+++ scripts/plot_interactive_ts.py
@@ -1,3 +1,10 @@
+"""
+@author Mike Smith
+@email michaesm@marine.rutgers.edu
+@Purpose To create an interactive time series plot for OOI data utilizing the bokeh module. This doesn't render an 
+@ actual plot, but rather an html containing interactiveSVG elements
+@ This WILL NOT work well for cabled OOI data. Try plot_interactive_ts_cabled.py for this.
+"""
 from bokeh.plotting import figure, output_file, reset_output, show, ColumnDataSource, save
 from bokeh.models import BoxAnnotation
 from functions import common as cf
@@ -8,8 +15,8 @@ import os
 
 tools = 'pan,zoom_in,zoom_out,wheel_zoom,box_zoom,reset,hover'
 
-# t1 = time.time()
-def main(nc, save_dir):
+
+def main(nc, save_dir, display=False):
     cf.create_dir(save_dir)
 
     with xr.open_dataset(nc, mask_and_scale=False) as ds:
@@ -20,26 +27,23 @@ def main(nc, save_dir):
         deployment = 'D0000{}'.format(str(np.unique(ds.deployment)[0]))
         t0 = ds.time_coverage_start
         t1 = ds.time_coverage_end
-        sub_dir = os.path.join(subsite, deployment, node, sensor, stream)
+        sub_dir = os.path.join(save_dir, subsite, deployment, node, sensor, stream)
 
         cf.create_dir(sub_dir)
 
         misc = ['quality', 'string', 'timestamp', 'deployment', 'id', 'provenance', 'qc', 'time', 'mission', 'obs',
                 'volt', 'ref', 'sig', 'amp', 'rph', 'calphase', 'phase', 'therm']
-        qc = ['qc_results', 'qc_executed']
-
         reg_ex = re.compile(r'\b(?:%s)\b' % '|'.join(misc))
 
         #  keep variables that are not in the regular expression
         vars = [s for s in ds.data_vars if not reg_ex.search(s)]
-        reg_ex = re.compile('|'.join(qc))
-        vars = [s for s in vars if not reg_ex.search(s)]
 
         x = ds['time'].data
 
         for v in vars:  # List of dataset variables
-            print v
-            if ds[v].dtype.kind == 'S' or ds[v].dtype == np.dtype('datetime64[ns]') or 'time' in v:
+            # print v
+            # Filter out variables that are strings, datetimes, or qc related
+            if ds[v].dtype.kind == 'S' or ds[v].dtype == np.dtype('datetime64[ns]') or 'time' in v or 'qc_results' in v or 'qc_executed' in v:
                 continue
             y = ds[v]
             try:
@@ -63,7 +67,7 @@ def main(nc, save_dir):
             output_file('{}/{}-{}-{}.html'.format(sub_dir, v, ds.time_coverage_start.replace(':', ''), ds.time_coverage_end.replace(':', '')))
 
             p = figure(width=1200,
-                       height=600,
+                       height=800,
                        title='{}-{}-{}: {} - {} - {}, Stream: {}'.format(subsite, node, sensor, deployment, t0, t1, stream),
                        x_axis_label='Time (GMT)', y_axis_label='{} ({})'.format(v, y_units),
                        x_axis_type='datetime',
@@ -77,11 +81,15 @@ def main(nc, save_dir):
                 p.add_layout(low_box)
                 p.add_layout(mid_box)
                 p.add_layout(high_box)
-            # save(p)
-            show(p)
+
+            if display:
+                show(p)
+            else:
+                save(p)
             reset_output()
 
 if __name__ == '__main__':
-    nc = 'https://opendap.oceanobservatories.org/thredds/dodsC/ooi/michaesm-marine-rutgers/20170317T160317-CE09OSSM-RID26-07-NUTNRB000-recovered_inst-nutnr_b_instrument_recovered/deployment0001_CE09OSSM-RID26-07-NUTNRB000-recovered_inst-nutnr_b_instrument_recovered_20150409T160033-20150525T210043.nc'
-    save_dir = '/Users/mikesmith/Documents/plots'
-    main(nc, save_dir)
+    file_name = 'https://opendap.oceanobservatories.org/thredds/dodsC/ooi/michaesm-marine-rutgers/20170317T160317-CE09OSSM-RID26-07-NUTNRB000-recovered_inst-nutnr_b_instrument_recovered/deployment0001_CE09OSSM-RID26-07-NUTNRB000-recovered_inst-nutnr_b_instrument_recovered_20150409T160033-20150525T210043.nc'
+    save = '/Users/mikesmith/Documents/plots'
+    display = True #set to True to display plots in browser as they are created
+    main(file_name, save, display)
